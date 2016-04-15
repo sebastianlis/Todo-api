8462462fc0699be4bbe0ca8f6de94f0f27c5b9cf
Sebastian Lis <sebastian.a.lis@gmail.com>
2016-04-15 20:58:39 +0200

    Add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -62,19 +64,28 @@ app.post('/todos', function(req, res)
 {
     var body = _.pick(req.body, 'description', 'completed');
 
-    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 )
+    db.todo.create(body).then(function(todo)
     {
-        return res.status(400).send();
-    }
+        res.json(todo.toJSON());
+    },
+    function(e)
+    {
+        res.status(400).json(e);
+    });
 
-    // set body.description to be trimmed value
-    body.description = body.description.trim();
+    //if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 )
+    //{
+    //    return res.status(400).send();
+    //}
 
-    body.id = todoNextId++;
+    //// set body.description to be trimmed value
+    //body.description = body.description.trim();
 
-    todos.push(body);
+    //body.id = todoNextId++;
 
-    res.json(body);
+    //todos.push(body);
+
+    //res.json(body);
 });
 
 //DELETE /todos/:id
@@ -130,8 +141,11 @@ app.put('/todos/:id', function(req, res)
     res.json(matchedTodo);
 });
 
-
-app.listen(PORT, function()
+db.sequelize.sync().then(function()
 {
-    console.log('Express listening on port ' + PORT + '!');
+    app.listen(PORT, function()
+    {
+        console.log('Express listening on port ' + PORT + '!');
+    });
 });
+
