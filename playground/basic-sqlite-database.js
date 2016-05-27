047d32a103bbbfce7e7e79d4bdbfef9ca0cbd897
Sebastian Lis <sebastian.a.lis@gmail.com>
2016-05-27 15:43:20 +0200

    Adding todo/user association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -21,64 +21,46 @@ var Todo = sequelize.define('todo',
     },
 });
 
+var User = sequelize.define('user', {
+        email: Sequelize.STRING
+})
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
 sequelize.sync({
-        //force: true
+    //force: true
 }).then(function()
 {
     console.log('Everything is synced');
-
-    Todo.findById(3).then(function(todo)
+    User.findById(1).then(function(user)
     {
-        if(todo)
+        user.getTodos({
+            where:{
+                completed: false
+            }
+        }).then(function(todos)
         {
-            console.log(todo.toJSON());
-        }
-        else
-        {
-            console.log('Todo not found');
-        }
+            todos.forEach(function(todo)
+            {
+                console.log(todo.toJSON());
+            });
+        });
     });
 
-    //Todo
-    //.create({
-    //    description: 'Take out trash'
-    //})
-    //.then(function(todo)
+    //User.create({
+    //        email: 'sebastian@example.com'
+    //}).then(function()
     //{
     //    return Todo.create({
-    //            description: 'Clean office'
+    //        description: 'Clean yard'
     //    });
-    //})
-    //.then(function()
+    //}).then(function(todo)
     //{
-    //    //return Todo.findById(1);
-    //    return Todo.findAll({
-    //            where: {
-    //                description:{
-    //                    $like : '%Office%'
-    //                }
-    //            }
+    //    User.findById(1).then(function(user)
+    //    {
+    //        user.addTodo(todo);
     //    });
-    //})
-    //.then(function(todos)
-    //{
-    //    if(todos)
-    //    {
-    //        todos.forEach(function(todo)
-    //        {
-    //            console.log(todo.toJSON());
-    //        });
-    //    }
-    //    else
-    //    {
-    //        console.log('no todo found!');
-    //    }
-    //})
-    //.catch(function(e)
-    //{
-    //    console.log(e);
     //});
-
-
 });
 
--- server.js
+++ server.js
@@ -79,7 +79,13 @@ app.post('/todos', middleware.requireAuthentication, function(req, res)
 
     db.todo.create(body).then(function(todo)
     {
-        res.json(todo.toJSON());
+        req.user.addTodo(todo).then(function()
+        {
+            return todo.reload();
+        }).then(function(todo)
+        {
+            res.json(todo.toJSON());
+        });
     },
     function(e)
     {
